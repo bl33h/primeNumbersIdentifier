958fa06bb5d00bfb60f83b785c6d60c5b5fe56ed
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-08-31 19:02:08 -0600

    function to determine if a number is prime works.

--- PrimeNumbersIdentifier.cpp
+++ PrimeNumbersIdentifier.cpp
@@ -4,5 +4,39 @@ Copyright (C), 2022-2023, Sara Echeverria (bl33h)
 FileName: PrimeNumbersIdentifier
 @version: I
 Creation: 25/08/2022
-Last modification: 25/08/2022
+Last modification: 31/08/2022
 ------------------------------------------------------------------------------*/
+// Imports
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <sstream>
+
+// Parameters to follow
+// --- Variables ---
+int boundary;
+int threads;
+int primesSum;
+// --- Threads info ---
+struct info
+{
+    int id;
+    int min; // *inferior limit
+    int max; // *superior limit
+};
+
+// Function to determine if a number is prime
+bool isPrime(int number) 
+{
+  if (number == 0 || number == 1 || number == 4) return false;
+  for (int n = 2; n < number / 2; n++) 
+  {
+    if (number % n == 0) return false;
+  }
+  return true;
+}
+
+// Summatory of all the prime numbers found on range
+
